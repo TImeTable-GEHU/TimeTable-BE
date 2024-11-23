b459d6236fa1ca63ec94f240d712d3fe2a61c742
Vibha <vibhachandola23012005@gmail.com>
2024-11-23 18:56:53 +0530

    Created Rooms apis

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("mongo-status/", views.mongo_status, name="mongo_status"),
-    path("postgres-status/", views.postgres_status, name="postgres_status"),
+    path("mongo-status/", views.mongo_status),
+    path("postgres-status/", views.postgres_status),
+    path("getRooms/", views.getRooms),
+    path("addRoom/", views.addRoom),
+    path("updateRoom/<int:pk>/", views.updateRoom),
+    path("deleteRoom/<int:pk>/", views.deleteRoom),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -3,6 +3,15 @@ from .db_drivers.mongodb_driver import MongoDriver
 from .db_drivers.postgres_driver import PostgresDriver
 import os
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Room
+from .serializers import RoomSerializer
+
+
+def generateTimetable():
+    pass
+
 
 def mongo_status(request):
     try:
@@ -29,3 +38,47 @@ def postgres_status(request):
         return JsonResponse({"postgresql": "Connected", "result": result})
     except Exception as e:
         return JsonResponse({"postgresql": "Not Connected", "error": str(e)})
+
+
+@api_view(["GET"])
+def getRooms(request):
+    rooms = Room.objects.all()
+    serializer = RoomSerializer(rooms, many=True)
+    return Response(serializer.data, status=200)
+
+
+@api_view(["POST"])
+def addRoom(request):
+    serializer = RoomSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        generateTimetable()
+        return Response(serializer.data, status=201)
+    else:
+        return Response(status=400)
+
+
+@api_view(["PUT"])
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    serializer = RoomSerializer(instance=room, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        generateTimetable()
+        return Response(serializer.data, status=200)
+
+    else:
+        return Response(status=400)
+
+
+@api_view(["DELETE"])
+def deleteRoom(request, pk):
+    try:
+        room = Room.objects.get(id=pk)
+        room.delete()
+        generateTimetable()
+        return Response(status=200)
+    except:
+        return Response(status=400)
