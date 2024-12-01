cdf6f8e3263c5443a7c0eb872810aa0966076d1c
Ayush-Techie <ayush.kapri@julofinance.com>
2024-12-01 14:57:05 +0530

    Refactor for classroom_allocate & section_allocate.

--- __init__.py
+++ __init__.py
@@ -32,8 +32,7 @@ def timetable_generation():
     )
 
     timetable = timetable_generator.create_timetable(Defaults.initial_no_of_chromosomes)
-    from icecream import ic
-    ic(timetable)
+
     # Fitness of each Chromosome
     fitness_calculator = TimetableFitnessEvaluator(
         timetable,
@@ -48,13 +47,14 @@ def timetable_generation():
         timetable_generator.weekly_workload,
     )
 
+    # Calc the fitness score.
     fitness_scores = fitness_calculator.evaluate_timetable_fitness()
 
 
     # Selection of all Chromosomes
     selection_object = TimeTableSelection()
     selected_chromosomes = selection_object.select_chromosomes(fitness_scores[1])
-    ic(len(selected_chromosomes))
+
 
     # Crossover for all selected Chromosomes
     crossover_object = TimeTableCrossOver()
@@ -81,11 +81,6 @@ def timetable_generation():
         for chromosome in crossover_chromosomes
     ]
 
-
-    ic(mutated_chromosomes)
-    ic(selected_chromosomes)
-
-    ic(selected_chromosomes)
     # Store best of Chromosomes
     best_chromosome_score = -1
     best_chromosome = dict()
@@ -95,13 +90,8 @@ def timetable_generation():
             best_chromosome_score = int(week_score)
             best_chromosome = timetable[week_no]
 
-    ic(f"Best Chromosome: {best_chromosome}")
-
 
 def run_timetable_generation():
     for generation in range(Defaults.total_no_of_generations):
         best_chromosome = timetable_generation()
     return best_chromosome
-
-
-run_timetable_generation()
